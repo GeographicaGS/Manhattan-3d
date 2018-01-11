211fcbeb0c46bd943efe7c347196190aa4434a30
cayetanobv <cayetano.benavent@geographica.gs>
2018-01-11 14:24:14 +0100

    updated queries

--- data/manhatan_pluto_dataset.sql
+++ data/manhatan_pluto_dataset.sql
@@ -1,6 +1,6 @@
-DROP TABLE IF EXISTS manhatan_pluto_14_17;
+DROP TABLE IF EXISTS manhattan_pluto_09_17;
 
-CREATE TABLE manhatan_pluto_14_17 As
+CREATE TABLE manhattan_pluto_09_17 As
 (
   (
     SELECT
@@ -49,8 +49,68 @@ CREATE TABLE manhatan_pluto_14_17 As
       AND (assesstot IS NOT NULL)
       AND (yearbuilt IS NOT NULL AND yearbuilt > 0)
   )
+  UNION ALL
+  (
+    SELECT
+        the_geom, numfloors * 4 as height,
+        yearbuilt, assesstot / bldgarea as assess_val_norm,
+        2013::integer as pub_date
+    FROM mnmappluto2013
+    WHERE NOT landuse IN ('09','07')
+      AND (bldgarea IS NOT NULL AND bldgarea > 0)
+      AND (assesstot IS NOT NULL)
+      AND (yearbuilt IS NOT NULL AND yearbuilt > 0)
+  )
+  UNION ALL
+  (
+    SELECT
+        the_geom, numfloors * 4 as height,
+        yearbuilt, assesstot / bldgarea as assess_val_norm,
+        2012::integer as pub_date
+    FROM mnmappluto2012
+    WHERE NOT landuse IN ('09','07')
+      AND (bldgarea IS NOT NULL AND bldgarea > 0)
+      AND (assesstot IS NOT NULL)
+      AND (yearbuilt IS NOT NULL AND yearbuilt > 0)
+  )
+  UNION ALL
+  (
+    SELECT
+        the_geom, numfloors * 4 as height,
+        yearbuilt, assesstot / bldgarea as assess_val_norm,
+        2011::integer as pub_date
+    FROM mnmappluto2011
+    WHERE NOT landuse IN ('09','07')
+      AND (bldgarea IS NOT NULL AND bldgarea > 0)
+      AND (assesstot IS NOT NULL)
+      AND (yearbuilt IS NOT NULL AND yearbuilt > 0)
+  )
+  UNION ALL
+  (
+    SELECT
+        the_geom, numfloors * 4 as height,
+        yearbuilt, assesstot / bldgarea as assess_val_norm,
+        2010::integer as pub_date
+    FROM mnmappluto2010
+    WHERE NOT landuse IN ('09','07')
+      AND (bldgarea IS NOT NULL AND bldgarea > 0)
+      AND (assesstot IS NOT NULL)
+      AND (yearbuilt IS NOT NULL AND yearbuilt > 0)
+  )
+  UNION ALL
+  (
+    SELECT
+        the_geom, numfloors * 4 as height,
+        yearbuilt, assesstot / bldgarea as assess_val_norm,
+        2009::integer as pub_date
+    FROM mnmappluto2009
+    WHERE NOT landuse IN ('09','07')
+      AND (bldgarea IS NOT NULL AND bldgarea > 0)
+      AND (assesstot IS NOT NULL)
+      AND (yearbuilt IS NOT NULL AND yearbuilt > 0)
+  )
 );
 
-SELECT CDB_CartodbfyTable('cayetano', 'manhatan_pluto_14_17');
+SELECT CDB_CartodbfyTable('cayetano', 'manhattan_pluto_09_17');
 
-CREATE INDEX mn_pluto14_17_yearb_idx ON manhatan_pluto_14_17(yearbuilt);
+CREATE INDEX mn_pluto14_17_yearb_idx ON manhattan_pluto_09_17(yearbuilt);
